9c9b1926741e24c6749cc5f68e9bfdffd80afccb
JIRAWAT WONGWICHAI <jirawat_wongwichai@cmu.ac.th>
2018-10-19 16:01:47 +0700

    success

--- TwoZeroFourEightView.cs
+++ TwoZeroFourEightView.cs
@@ -14,6 +14,7 @@ namespace twozerofoureight
     {
         Model model;
         Controller controller;
+        int score;
        
         public TwoZeroFourEightView()
         {
@@ -23,11 +24,16 @@ namespace twozerofoureight
             controller = new TwoZeroFourEightController();
             controller.AddModel(model);
             controller.ActionPerformed(TwoZeroFourEightController.LEFT);
+            KeyDown += new KeyEventHandler(OnKeyDownHandler);
         }
 
         public void Notify(Model m)
         {
             UpdateBoard(((TwoZeroFourEightModel) m).GetBoard());
+            if (m.isEnd)
+            {
+                EndGame();
+            }
         }
 
         private void UpdateTile(Label l, int i)
@@ -52,8 +58,30 @@ namespace twozerofoureight
                 case 8:
                     l.BackColor = Color.Red;
                     break;
-                default:
+                case 16:
                     l.BackColor = Color.Green;
+                    break;
+                case 32:
+                    l.BackColor = Color.MediumPurple;
+                    break;
+                case 64:
+                    l.BackColor = Color.MediumSpringGreen;
+                    break;
+                case 128:
+                    l.BackColor = Color.MediumSlateBlue;
+                    break;
+                case 256:
+                    l.BackColor = Color.Moccasin;
+                    break;
+                case 512:
+                    l.BackColor = Color.Olive;
+                    break;
+                case 1024:
+                    l.BackColor = Color.MistyRose;
+                    break;
+
+                default:
+                    l.BackColor = Color.Navy;
                     break;
             }
         }
@@ -75,27 +103,43 @@ namespace twozerofoureight
             UpdateTile(lbl31,board[3, 1]);
             UpdateTile(lbl32,board[3, 2]);
             UpdateTile(lbl33,board[3, 3]);
+
+            score = 0;
+            label3.Text = "0";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    score += board[i, j];
+                }
+            }
+            label3.Text = score.ToString();
+        }
+        
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {            
+            if (e.KeyCode == Keys.Left)
+            {
+                controller.ActionPerformed(TwoZeroFourEightController.LEFT);
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                controller.ActionPerformed(TwoZeroFourEightController.UP);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                controller.ActionPerformed(TwoZeroFourEightController.RIGHT);
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                controller.ActionPerformed(TwoZeroFourEightController.DOWN);
+            }
         }
 
-        private void btnLeft_Click(object sender, EventArgs e)
-        {
-            controller.ActionPerformed(TwoZeroFourEightController.LEFT);
+        private void EndGame()
+        {            
+            MessageBox.Show("GAME OVER!");
         }
-
-        private void btnRight_Click(object sender, EventArgs e)
-        {
-            controller.ActionPerformed(TwoZeroFourEightController.RIGHT);
-        }
-
-        private void btnUp_Click(object sender, EventArgs e)
-        {
-            controller.ActionPerformed(TwoZeroFourEightController.UP);
-        }
-
-        private void btnDown_Click(object sender, EventArgs e)
-        {
-            controller.ActionPerformed(TwoZeroFourEightController.DOWN);
-        }
-
     }
 }
